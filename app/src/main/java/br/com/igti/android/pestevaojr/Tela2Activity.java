4be6e3eee5fd045cf821d850429e11a990fd21e4
Paulo Estêvão <paulo.estevao@tst.jus.br>
2019-10-20 11:03:10 -0300

    Primeiro commit.

---
+++ app/src/main/java/br/com/igti/android/pestevaojr/Tela2Activity.java
@@ -0,0 +1,14 @@
+package br.com.igti.android.pestevaojr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela2);
+    }
+}
